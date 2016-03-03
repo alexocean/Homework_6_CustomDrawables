3e7ae493a92a2512e1ff10f6cf8f8d3ef104be4c
alexocean <o.b.myronyuk@gmail.com>
2016-03-03 09:55:19 +0300

    Initial commit

---
+++ app/src/main/java/guruthinkmobile/com/homework_6_customdrawables/MainActivity.java
@@ -0,0 +1,13 @@
+package guruthinkmobile.com.homework_6_customdrawables;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
